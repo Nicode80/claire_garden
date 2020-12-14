7124c2ac7ee37804fac902022e1d660c88f8893a
Nicolas Constantin <nicolasconstantin@hotmail.fr>
2020-12-14 23:29:42 +0100

    create done on index + route controller and view file for the show of plant

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -3,6 +3,30 @@ class PlantsController < ApplicationController
   def index
     @plants = Plant.where(user_id: current_user.id)
     @user = current_user
+    @plant = Plant.new
   end
 
+  def create
+    @plant = Plant.new(plant_params)
+    @user = current_user
+    @plant.user = @user
+    if @plant.save
+      flash[:notice] = "Plante ajoutée"
+      redirect_to plant_path(@plant)
+    end
+  end
+
+  def show
+    @plant = Plant.find(params[:id])
+  end
+
+  private
+
+  def plant_params
+    params.require(:plant).permit(
+      :name,
+      :notes,
+      :category,
+      :photo)
+  end
 end
