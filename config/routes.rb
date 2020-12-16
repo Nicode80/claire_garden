db395abd1de0069ffd5c73eaafe40cf831f264b9
Nicolas Constantin <nicolasconstantin@hotmail.fr>
2020-12-16 16:12:01 +0100

    add route and method to create task + redirect to root if not authenticated

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :plants, only: [:index, :create, :show, :update]
+  resources :plants, only: [:index, :create, :show, :update] do
+    resources :tasks, only: [:create]
+  end
 end
