db930433143df5aee36fadf9248377b2b5b8d93e
Nicolas Constantin <nicolasconstantin@hotmail.fr>
2020-12-24 17:37:54 +0100

    new sign in page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,13 +17,4 @@ class ApplicationController < ActionController::Base
 
   protected
 
-  def authenticate_user!
-    if user_signed_in?
-      super
-    else
-      redirect_to root_path, notice: 'Vous devez être connecté'
-      ## if you want render 404 page
-      ## render :file => File.join(Rails.root, 'public/404'), :formats => [:html], :status => 404, :layout => false
-    end
-  end
 end
