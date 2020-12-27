648225c4df000c3947415f8b2828a72a73836dd7
Nicolas Constantin <nicolasconstantin@hotmail.fr>
2020-12-27 22:29:22 +0100

    change root to plants index

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users,
     controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
-  devise_scope :user do
-    root to: "devise/sessions#new"
-  end
+  # devise_scope :user do
+  #   root to: "devise/sessions#new"
+  # end
+  root to: "plants#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :plants, only: [:index, :create, :show, :update] do
