e3c106b334039855a9f014d10fa5996bd92e9aaf
Nicolas Constantin <nicolasconstantin@hotmail.fr>
2020-12-27 17:16:07 +0100

    get rid of useless pages files

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users,
     controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
-  root to: 'pages#home'
+  devise_scope :user do
+    root to: "devise/sessions#new"
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :plants, only: [:index, :create, :show, :update] do
